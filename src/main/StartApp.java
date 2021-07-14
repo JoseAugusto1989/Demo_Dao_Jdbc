e9f82a29fbd6f6509badd824041c6ffa26a2696e
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2021-07-14 20:19:26 -0300

    Class Seller

--- src/main/StartApp.java
+++ src/main/StartApp.java
@@ -1,13 +1,19 @@
 package main;
 
+import java.util.Date;
+
 import model_entities.Department;
+import model_entities.Seller;
 
 public class StartApp {
 	
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1, "Books");
-		System.out.println(dep);
+		
+		Seller sel = new Seller(21, "Marcos", "Marc@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(sel);
 		
 	}
 
