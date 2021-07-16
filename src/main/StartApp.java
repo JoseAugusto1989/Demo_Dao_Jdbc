89d6808fc5284d5f8fc2ada6689e0893a8f84d60
Jose Augusto de Oliveira Pins <64021880+JoseAugusto1989@users.noreply.github.com>
2021-07-15 22:54:19 -0300

    Implementaçao findByDepartment

--- src/main/StartApp.java
+++ src/main/StartApp.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.Date;
+import java.util.List;
 
 import model_dao.DaoFactory;
 import model_dao.SellerDao;
@@ -15,8 +16,16 @@ public class StartApp {
 		
 		System.out.println("\n-------TEST01: seller findById-------");
 		Seller seller = sellerDao.findById(2);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n-------TEST02: seller findByDepartment-------");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 		
 	}
 
